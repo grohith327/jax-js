c011f5318d48c32e30151d9e9554a8d9337e5edf
Eric Zhang <ekzhang1@gmail.com>
2025-12-08 10:16:41 -0800

    Add vite-plugin-lucide-preprocess for faster builds

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -2,11 +2,13 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 import { defineConfig } from "vite";
+import lucidePreprocess from "vite-plugin-lucide-preprocess";
 
 export default defineConfig({
   plugins: [
     sveltekit(),
     tailwindcss(),
+    lucidePreprocess(),
     process.env.BASIC_SSL ? basicSsl() : null,
   ],
   optimizeDeps: {
